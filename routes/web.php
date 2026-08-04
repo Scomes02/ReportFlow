661d52feb54975d47eea3fcb09271311811b1414
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-08-04 01:15:03 -0300

    agregamos ruta

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Rutas del Médico
+Route::get('/medico', [StudyController::class, 'index'])->name('medico.index');
+Route::put('/medico/studies/{id}/report', [StudyController::class, 'processReport'])->name('medico.processReport');
