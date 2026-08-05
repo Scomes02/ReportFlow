cffaa608ac630e186b1781b46493d5b0156df266
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-08-04 23:12:06 -0300

    agregue rutas para modulo-medico

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MedicoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-// Rutas del Médico
-Route::get('/medico', [StudyController::class, 'index'])->name('medico.index');
-Route::put('/medico/studies/{id}/report', [StudyController::class, 'processReport'])->name('medico.processReport');
+// Bloque de rutas para modulo-medico
+
+Route::prefix('medico')->name('medico.')->group(function () {
+    Route::get('/estudios', [MedicoController::class, 'index'])->name('estudios.index');
+    Route::post('/estudios/{id}/informar', [MedicoController::class, 'guardarInforme'])->name('estudios.informar');
+});
